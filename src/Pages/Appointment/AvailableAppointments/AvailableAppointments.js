3202485e785a14c81524fa57b651203aaab6712b
Supriya debnath <supriya.cucse@gmail.com>
2022-02-11 03:32:34 +0600

    booking modal

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -5,39 +5,39 @@ import Booking from '../Booking/Booking';
 const bookings =[ 
 {
     id: 1,
-    name: 'Teeth Arthodonies',
-    time: '8.00 am - 9. 00 am',
+    name: 'Teeth Orthodonics',
+    time: '8.00 am - 9.00 am',
     space: 10,
 },
 {
-    id: 1,
-    name: 'Teeth Arthodonies',
-    time: '8.00 am - 9. 00 am',
+    id: 2,
+    name: 'Cosmetic Dentistry',
+    time: '10.00 am - 11.00 am',
+    space: 20,
+},
+{
+    id: 3,
+    name: 'Teeth Cleaning',
+    time: '12.00 am - 1.00 am',
+    space: 17,
+},
+{
+    id: 4,
+    name: 'Cavity Protection',
+    time: '4.00 am - 5.00 am',
     space: 10,
 },
 {
-    id: 1,
-    name: 'Teeth Arthodonies',
-    time: '8.00 am - 9. 00 am',
-    space: 10,
+    id: 5,
+    name: 'Pediatric Dental',
+    time: '6.00 am - 7.00 am',
+    space: 14,
 },
 {
-    id: 1,
-    name: 'Teeth Arthodonies',
+    id: 6,
+    name: 'Oral Surgery',
     time: '8.00 am - 9. 00 am',
-    space: 10,
-},
-{
-    id: 1,
-    name: 'Teeth Arthodonies',
-    time: '8.00 am - 9. 00 am',
-    space: 10,
-},
-{
-    id: 1,
-    name: 'Teeth Arthodonies',
-    time: '8.00 am - 9. 00 am',
-    space: 10,
+    space: 7,
 }
 ]
 
@@ -45,13 +45,14 @@ const AvailableAppointments = ({date}) => {
     
     return (
         <div>
-            <Typography variant='h4' sx={{color: 'info.main', mb: 3}}>Available Appointments {date.toDateString()}
+            <Typography variant='h4' sx={{color: 'info.main', mb: 3}}>Available Appointments On <em sx={{color: '#E80D42'}}>{date.toDateString()}</em>
             </Typography>
             <Grid container spacing={2}>
                 {
                     bookings.map(booking => <Booking
                     key={booking.id}
                     booking={booking}
+                    date={date}
                     ></Booking>)
                 }
                 
--- src/Pages/Appointment/Booking/Booking.js
+++ src/Pages/Appointment/Booking/Booking.js
@@ -2,12 +2,19 @@ import React from 'react';
 import { Button, Grid } from '@mui/material';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
+import BookingModal from '../BookingModal/BookingModal';
 
 
-const Booking = ({booking}) => {
+const Booking = ({booking, date}) => {
     const {name, time, space} = booking;
+
+    const [openBooking, setBookingOpen] = React.useState(false);
+    const handleBookingOpen = () => setBookingOpen(true);
+    const handleBookingClose = () => setBookingOpen(false);
+
     return (
-        <Grid item xs={12} md={4} sm={6}>
+        <>
+            <Grid item xs={12} md={4} sm={6}>
             <Paper elevation={3} sx={{py: 5}}>
             <Typography sx={{color: 'info.main', fontWeight: '600'}} variant="h5" gutterBottom component="div">
                 {name}
@@ -18,10 +25,19 @@ const Booking = ({booking}) => {
             <Typography variant="caption" display="block" gutterBottom>
                 {space} SPACES AVAILABLE
             </Typography>
-            <Button style={{marginTop: '20px', backgroundColor: '#21C6BC'}} variant="contained">Book Appointment</Button>
+            <Button onClick={handleBookingOpen}  style={{marginTop: '20px', backgroundColor: '#21C6BC'}} variant="contained">Book Appointment</Button>
             </Paper>
 
          </Grid>
+         <BookingModal
+         date={date}
+         booking={booking}
+         openBooking={openBooking}
+         handleBookingClose={handleBookingClose}
+         >
+         </BookingModal>
+        </>
+         
     );
 };
 
